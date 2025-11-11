98104343e6313ad093e9152694309592e6857d8a
MiKL <miklgaillard@gmail.com>
2025-11-11 19:55:58 +0100

    ✨ Scrapy ➜ use xpath

--- projects/books2scrape/books2scrape/spiders/b2s.py
+++ projects/books2scrape/books2scrape/spiders/b2s.py
@@ -8,21 +8,44 @@ class B2sSpider(scrapy.Spider):
 
     def parse(self, response):
         books = response.xpath('//li[@class="col-xs-6 col-sm-4 col-md-3 col-lg-3"]')[0],
-        book_title = response.xpath('.//h3/a/text()').get(),
-        book_price = response.xpath('.//p[@class="price_color"]/text()').get()
-        
+        # book_title  = response.xpath('.//h3/a/text()').get(),
+        # book_price  = response.xpath('.//p[@class="price_color"]/text()').get()
+        # books_title = response.xpath("//h3[contains(.//text(),'the')]")
+        # text_books  = books_title.xpath('.//text()').getall()
+        # category_bloc = response.xpath('//div[@class="side_categories"]/ul/li/ul/li')
+        # text_category = category_bloc.xpath('normalize-space(.//a/text())').getall()
+        # div_image         = response.xpath('.//div[@class="image_container"]')[0]
+        # div_image_child   = div_image.xpath('.//child::node()')
+        # div_image_sibling = div_image.xpath('.//following-sibling::node()')
+        # div_image_preview = div_image.xpath('.//preview-sibling::node()')
+        # div_image_parents = div_image.xpath('.//encestor::node()')
+        for bloc in books:
+            yield {
+                'title'  : bloc.xpath('.//h3/a/@title').get(),
+                'price'  : bloc.xpath('.//p[@class=price_color"]/text()').get(),
+                'img_src': bloc.path('.//img/src').get
+            }
+
         result = {
-            'h1'    : response.xpath('//h1'),
-            'li'    : response.xpath('//li'),
-            'title' : response.xpath('//head/title/text()').get(),
-            'links' : response.xpath('//a/text()').getall(),
-            'altert': response.xpath('//div[@role="alert"]/text()').get(),
-            'price' : response.xpath('//p[@class="price_color"]/text()').getall(),
-            'book'  : books,
-            'title1': book_title,
-            'price1': book_price,
-            'url'   : books.xpath('//h3/a/@href').get(),
-            'title2': books.xpath('//h3/a/@title').get()
+            # 'h1'    : response.xpath('//h1'),
+            # 'li'    : response.xpath('//li'),
+            # 'title' : response.xpath('//head/title/text()').get(),
+            # 'links' : response.xpath('//a/text()').getall(),
+            # 'altert': response.xpath('//div[@role="alert"]/text()').get(),
+            # 'price' : response.xpath('//p[@class="price_color"]/text()').getall(),
+            # 'book'  : books,
+            # 'title1': book_title,
+            # 'price1': book_price,
+            # 'url'   : books.xpath('//h3/a/@href').get()
+            # 'title2': books.xpath('//h3/a/@title').get()
+            # 'links_categories': response.xpath("//a[contains(@href,'/category')]")
+            # 'title3': books_title
+            # 'title4': text_books
+            # 'categorie': text_category
+            # 'div'     : div_image_child
+            # 'div' : div_image_sibling
+            # 'div' : div_image_preview
+            # 'div ' : div_image_parents
         }
         yield result
 
@@ -30,3 +53,6 @@ class B2sSpider(scrapy.Spider):
 # + la méthode text() et get() pour récupérer
 # getall() S'il y a plusieurs valeurs
 # [0] Si je veux le premier ou get()
+# .//text() est un référence relative pour le texte
+# following-sibling prend balise suivante de m^me niveau
+# preview-sibling prend celle d'avant
---
+++ projects/books2scrape/readme.md
@@ -0,0 +1,25 @@
+# Web Scraping de livres sur books.toscrape.com avec Scrapy<a href="../../"><img align="right" src="../../assets/atomicWebScraping.png" alt="Web scraping" height="64px"></a>
+<div align="center">
+
+![Python](https://img.shields.io/badge/python-3.13-blue?style=flat&logo=python&logoColor=FFD43B) 
+![Scrapy](https://img.shields.io/badge/Scrapy-Web_Scraping-5E8862?style=flat&logo=scrapy&logoColor=white)
+
+</div><hr>
+
+Il s'agit d'un spider Scrapy visant à extraire automatiquement des informations bibliographiques (titre, prix, image, ...) depuis le site de démonstration [books.toscrape.com](https://books.toscrape.com/).  
+Le but est de collecter et structurer les données pour chaque livre d’une page principale.
+---
+## **Le fonctionnement**
+1. **Collecte des blocs de livres** avec la requête XPath sur la classe de chaque livre (`//li[@class="col-xs-6 col-sm-4 col-md-3 col-lg-3"]`).
+2. **Extraction des informations** pour chaque livre :
+   - Titre (`.//h3/a/@title`)
+   - Prix (`.//p[@class="price_color"]/text()`)
+   - Source de l’image (`.//img/@src`)
+3. **Astuces XPath** documentées dans le fichier pour manipuler descendants, attributs, siblings, etc.
+## **Remarques**
+- La syntaxe `.//` dans les XPaths cible des descendants à partir d’un noeud local (ex: un bloc livre).
+- Les méthodes `.get()`, `.getall()`, `.extract()`, etc., selon le nombre de valeurs attendues.
+- Pour extraire « le premier » élément, utiliser `[0]` ou `.get()` ; pour tous, `.getall()`.
+## **Références**
+- [Documentation officielle Scrapy](https://docs.scrapy.org/)
+- [XPath Cheatsheet](https://devhints.io/xpath)
